a8e134f8ed33ee85a1ae07e6fddbd93b6cff296a
vaga1529 <134681255+vaga1529@users.noreply.github.com>
2023-05-27 01:28:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,71 @@ $(document).ready(function () {
     })
   }
 })
+
+const submitButton = document.getElementById('submitButton')
+const postTitle = document.getElementById('postTitle')
+const postBody = document.getElementById('postBody')
+const postsDiv = document.getElementById('posts')
+const postViewer = document.getElementById('postViewer')
+const viewTitle = document.getElementById('viewTitle')
+const viewBody = document.getElementById('viewBody')
+
+submitButton.addEventListener('click', function () {
+  const title = postTitle.value
+  const body = postBody.value
+
+  if (!title || !body) {
+    alert('제목과 내용을 모두 입력해주세요!')
+    return
+  }
+
+  const newPostKey = firebase.database().ref().child('posts').push().key
+  firebase
+    .database()
+    .ref('posts/' + newPostKey)
+    .set({
+      title: title,
+      body: body,
+    })
+
+  postTitle.value = ''
+  postBody.value = ''
+})
+
+function createPostElement(postId, post) {
+  const postElement = document.createElement('div')
+  postElement.onclick = function () {
+    viewPost(postId)
+  }
+
+  const postTitle = document.createElement('h4')
+  postTitle.textContent = post.title
+  postElement.appendChild(postTitle)
+
+  return postElement
+}
+
+function displayPosts(posts) {
+  postsDiv.innerHTML = ''
+
+  for (const postId in posts) {
+    const postElement = createPostElement(postId, posts[postId])
+    postsDiv.appendChild(postElement)
+  }
+}
+
+function viewPost(postId) {
+  const postRef = firebase.database().ref('posts/' + postId)
+  postRef.on('value', function (snapshot) {
+    postViewer.style.display = 'block'
+    viewTitle.textContent = snapshot.val().title
+    viewBody.textContent = snapshot.val().body
+  })
+}
+
+firebase
+  .database()
+  .ref('posts/')
+  .on('value', function (snapshot) {
+    displayPosts(snapshot.val())
+  })
